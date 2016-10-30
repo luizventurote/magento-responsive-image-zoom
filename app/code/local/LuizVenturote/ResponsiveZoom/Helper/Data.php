f4f590557d1f72ebd02d3d432a9cf7bcea6669d1
luizventurote <luiz@randwmedia.com>
2016-10-30 00:33:43 -0200

    Adds initial files

---
+++ app/code/local/LuizVenturote/ResponsiveZoom/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Luiz Venturote - Responsive Zoom
+ *
+ * @author  Luiz Venturote <luiz@randwmedia.com>
+ * @package LuizVenturote_ResponsiveZoom
+ */
+class LuizVenturote_ResponsiveZoom_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
